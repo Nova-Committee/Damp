e9a9ff876ecad0fa747749481917ff6de334d9ed
MikhailTapio <mikhailtapio13468@163.com>
2022-05-15 01:14:31 +0800

    Optimization

--- src/main/java/committee/nova/damp/mixin/MixinPrimedTnt.java
+++ src/main/java/committee/nova/damp/mixin/MixinPrimedTnt.java
@@ -1,9 +1,9 @@
 package committee.nova.damp.mixin;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,30 +11,26 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Creeper.class)
-public abstract class MixinCreeper extends LivingEntity {
+@Mixin(PrimedTnt.class)
+public abstract class MixinPrimedTnt extends Entity {
     @Shadow
-    private int maxSwell;
+    public abstract int getFuse();
 
     @Shadow
-    private int swell;
+    public abstract void setFuse(int p_32086_);
 
-    @Shadow
-    private int oldSwell;
-
-    protected MixinCreeper(EntityType<? extends LivingEntity> e, Level w) {
+    public MixinPrimedTnt(EntityType<?> e, Level w) {
         super(e, w);
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
     public void onTick(CallbackInfo ci) {
-        if (!this.isAlive()) return;
+        final int fuse = getFuse();
         if (!level.isRainingAt(new BlockPos(getEyePosition().add(0, 1, 0)))) {
-            if (maxSwell <= 30 || (random.nextInt(2) & 1) == 0) return;
-            if (maxSwell - swell > 30) maxSwell--;
+            if (fuse <= 80 || (random.nextInt(2) & 1) == 0) return;
+            setFuse(fuse - 1);
         } else {
-            if (maxSwell < 2000) maxSwell++;
-            if (swell % 120 == 119) swell = 0;
+            if (fuse < 2000) setFuse(fuse + 1);
         }
     }
 }
