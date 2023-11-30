1a5ebb6902296eb5b852fa80c8bc2dca477d1812
MikhailTapio <mikhailtapio13468@163.com>
2023-11-30 11:37:11 +0800

    Port to Forge 1.20.1

---
+++ src/main/java/committee/nova/damp/Damp.java
@@ -0,0 +1,35 @@
+package committee.nova.damp;
+
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+
+@Mod(Damp.MODID)
+public class Damp {
+    public static final String MODID = "damp";
+    public static final ForgeConfigSpec COMMON_CONFIG;
+    public static final ForgeConfigSpec.BooleanValue creeperDampness;
+    public static final ForgeConfigSpec.BooleanValue tntDampness;
+    public static final ForgeConfigSpec.IntValue creeperMaxSwell;
+    public static final ForgeConfigSpec.IntValue tntMaxFuse;
+
+    static {
+        final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        builder.comment("QuickPlant Configuration");
+        creeperDampness = builder.comment("Damp Configuration", "Activate creeper dampness")
+                .define("creeper_dampness", true);
+        tntDampness = builder.comment("Activate TNT dampness")
+                .define("tnt_dampness", true);
+        creeperMaxSwell = builder.comment("What's the max fuse time when the creeper reaches his max dampness")
+                .defineInRange("creeper_swell", 2000, 100, Integer.MAX_VALUE);
+        tntMaxFuse = builder.comment("What's the max fuse time when the primed tnt reaches his max dampness")
+                .defineInRange("tnt_fuse", 2000, 100, Integer.MAX_VALUE);
+        COMMON_CONFIG = builder.build();
+    }
+
+    public Damp() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, COMMON_CONFIG);
+    }
+}
---
+++ src/main/java/committee/nova/damp/mixin/MixinCreeper.java
@@ -0,0 +1,39 @@
+package committee.nova.damp.mixin;
+
+import committee.nova.damp.Damp;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Creeper.class)
+public abstract class MixinCreeper extends LivingEntity {
+    @Shadow
+    private int maxSwell;
+
+    @Shadow
+    private int swell;
+
+    protected MixinCreeper(EntityType<? extends LivingEntity> e, Level w) {
+        super(e, w);
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    public void onTick(CallbackInfo ci) {
+        if (!Damp.creeperDampness.get()) return;
+        if (!this.isAlive()) return;
+        if (getPersistentData().getBoolean("no_dampness")) return;
+        if (!isInWaterOrRain()) {
+            if (maxSwell <= 30 || random.nextBoolean()) return;
+            if (maxSwell - swell > 30) maxSwell--;
+            return;
+        }
+        if (maxSwell < Damp.creeperMaxSwell.get()) maxSwell++;
+        if (swell % 120 == 119) swell = 0;
+    }
+}
